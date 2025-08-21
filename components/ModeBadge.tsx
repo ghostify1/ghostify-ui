a7346872dc9fef37693afc2caf06bc5e5fdf9846
ghostify1 <ghostify.sprt@gmail.com>
2025-08-21 22:17:41 +0300

    Delete components directory

--- components/ModeBadge.tsx
+++
@@ -1,3 +0,0 @@
-export default function ModeBadge({ mode }:{mode:"demo"|"live"}){
-  return <span className={`badge fixed right-4 top-4 ${mode==="live"?"text-black bg-emerald-300 border-emerald-400":"text-black bg-zinc-300 border-zinc-400"}`}>{mode.toUpperCase()}</span>
-}
