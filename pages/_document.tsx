fe6bb2e9f29622eaabfd2185dc2b16f0783bb4f2
ghostify1 <ghostify.sprt@gmail.com>
2025-08-25 14:42:03 +0300

    Delete pages directory

--- pages/_document.tsx
+++
@@ -1,4 +0,0 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-export default function Document(){return(<Html lang='tr'><Head><meta name='theme-color' content='#0B1115'/>
-<link rel='preconnect' href='https://fonts.googleapis.com'/><link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin=''/>
-<link href='https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap' rel='stylesheet'/></Head><body><Main/><NextScript/></body></Html>);}
