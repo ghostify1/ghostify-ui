f58573b39bbc9cddf3f36d7dbff1d678b23073d2
ghostify1 <ghostify.sprt@gmail.com>
2025-09-01 14:15:28 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import type { AppProps } from 'next/app'
-import '@/styles/globals.css'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import AppLayout from "../components/layout/AppLayout";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
+  );
 }
