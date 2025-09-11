6db54644ffc9a7a169105e830032928ceeebf26b
ghostify1 <ghostify.sprt@gmail.com>
2025-09-12 02:21:41 +0300

    Delete components directory

--- components/NavLogo.tsx
+++
@@ -1,8 +0,0 @@
-import Link from 'next/link'; import Image from 'next/image';
-export default function NavLogo(){ return(<div className='fixed left-4 top-4 z-10'>
-  <Link href='/' aria-label='Ghostify Ana Sayfa'>
-    <div className='w-12 h-12 relative rounded-xl border border-cyan-400/50 bg-cyan-400/10 hover:bg-cyan-400/20 transition'>
-      <Image src='/ghostify-logo.png' alt='Ghostify' fill sizes='48px' style={{objectFit:'contain'}}/>
-    </div>
-  </Link>
-</div>); }
