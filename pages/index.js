32ca9ad4f1c6c2df0a69b4021928ccbf81a6c4a1
ghostify1 <ghostify.sprt@gmail.com>
2025-09-22 03:14:03 +0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Ghostify PR1–4 Başarılı!</h1>
-      <p>Tüm sistem modülleri doğru çalışıyor.</p>
-    </div>
-  );
-}
