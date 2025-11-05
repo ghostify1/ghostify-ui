8914cc4ab69fdb11b5a33343e378438cfa27b8e4
ghostify1 <ghostify.sprt@gmail.com>
2025-11-05 03:28:04 +0300

    Update _app.js

--- _app.js
+++ _app.js
@@ -1,5 +1,11 @@
 import "../styles/globals.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  // Matrix efektine ufak fade animasyonu ekliyoruz
+  useEffect(() => {
+    document.body.style.backgroundColor = "#05080b";
+  }, []);
+
   return <Component {...pageProps} />;
 }
