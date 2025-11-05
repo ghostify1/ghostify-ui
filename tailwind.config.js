81e5f0f2b597e8b42c90e0c4ccb579285e7a8ec5
ghostify1 <ghostify.sprt@gmail.com>
2025-11-05 03:27:47 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,19 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         cyan: {
           400: "#25E6FF",
-          300: "#72F6FF"
-        }
+          300: "#72F6FF",
+        },
       },
-      animation: {
-        "spin-slow": "spin 12s linear infinite"
-      }
+      fontFamily: {
+        orbitron: ["Orbitron", "sans-serif"],
+      },
     },
   },
   plugins: [],
