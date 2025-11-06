5bca87bf3d3370350a43f2d0b4bdb6ff9dbaef28
ghostify1 <ghostify.sprt@gmail.com>
2025-11-06 20:15:46 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
+  typescript: { ignoreBuildErrors: true },
+  output: "standalone"  // 🔥 ekledik
 };
 module.exports = nextConfig;
