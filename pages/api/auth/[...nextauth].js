712f081b6910106d631b5b8fe520cb9f36a329f6
ghostify1 <ghostify.sprt@gmail.com>
2025-11-06 19:23:37 +0300

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,18 @@
 import NextAuth from "next-auth";
-import { authOptions } from "../../../lib/authOptions";
+import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+});
