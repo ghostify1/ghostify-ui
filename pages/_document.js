ba6b968f1f9f6a82dbbe788accac353a6ab8c512
ghostify1 <ghostify.sprt@gmail.com>
2025-11-12 00:51:43 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,4 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
-
 export default function Document(){
   return (
     <Html lang="tr">
@@ -10,10 +8,7 @@ export default function Document(){
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet" />
       </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+      <body><Main /><NextScript /></body>
     </Html>
   );
 }
