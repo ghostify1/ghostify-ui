472fbab162d6bc91d00fc4c4b8f3e11715e472c5
ghostify1 <ghostify.sprt@gmail.com>
2025-11-20 01:41:02 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 // pages/_app.js
 import "../styles/global.css";
+import dynamic from "next/dynamic";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// Matrix sadece client’ta çalışsın diye dynamic import
+const MatrixBackground = dynamic(
+  () => import("../components/MatrixBackground"),
+  { ssr: false }
+);
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div className="app-root">
+      <MatrixBackground />
+      <Component {...pageProps} />
+    </div>
+  );
 }
-
-export default MyApp;
