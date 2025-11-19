8aa49da2d83e278255380c9994d4bba92a33074c
ghostify1 <ghostify.sprt@gmail.com>
2025-11-19 22:16:21 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 // pages/_app.js
 import "../styles/global.css";
+import "../styles/invite.css";
+import "../styles/login.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
