07656350fbdc612bd19b2290a8ebb360ca26c85d
ghostify1 <ghostify.sprt@gmail.com>
2025-12-15 16:03:41 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,57 +4,16 @@ export default function Document() {
   return (
     <Html lang="tr">
       <Head>
-
-        {/* --- META --- */}
-        <meta charSet="UTF-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
-        <meta name="theme-color" content="#020510" />
-        <meta name="description" content="Ghostify - Dijital İz Güvenliği Platformu" />
-
-        {/* --- FAVICON + MANIFEST --- */}
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-
-        {/* --- GHOSTIFY FONTS --- */}
+        {/* Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-          crossOrigin=""
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-
-        {/* Orbitron (Logo + Başlıklar) */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=JetBrains+Mono:wght@400;600&family=Space+Grotesk:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
-
-        {/* Exo 2 (Input + Buton tipografi) */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Exo+2:wght@300;400;600&display=swap"
-          rel="stylesheet"
-        />
-
-        {/* JetBrains Mono (Matrix fontu + teknik text) */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;500;700&display=swap"
-          rel="stylesheet"
-        />
-
-        {/* --- PERFORMANCE OPTIMIZATION --- */}
-        <link rel="preload" href="/matrix-texture.png" as="image" />
-
+        <meta name="theme-color" content="#05070B" />
       </Head>
-
       <body>
-        {/* Matrix Background her sayfaya uygulanır */}
         <Main />
         <NextScript />
       </body>
